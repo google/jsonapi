c7fa0d5728fb0c08c66136547be30caf3b3c430a
Sam Woodard <samwoodard@nestlabs.com>
2015-07-12 10:46:51 -0700

    reorg, add list blogs impl

--- examples/app.go
+++ examples/app.go
@@ -13,6 +13,36 @@ import (
 	"github.com/shwoodard/jsonapi"
 )
 
+func createBlog(w http.ResponseWriter, r *http.Request) {
+	blog := new(Blog)
+
+	if err := jsonapi.UnmarshalPayload(r.Body, blog); err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	// ...do stuff with your blog ...
+
+	w.WriteHeader(201)
+	w.Header().Set("Content-Type", "application/vnd.api+json")
+
+	if err := jsonapi.MarshalOnePayload(w, blog); err != nil {
+		http.Error(w, err.Error(), 500)
+	}
+}
+
+func listBlogs(w http.ResponseWriter, r *http.Request) {
+	// ... fetch your blogs and filter, offset, limit, etc ...
+
+	blogs := testBlogsForList()
+
+	w.WriteHeader(200)
+	w.Header().Set("Content-Type", "application/vnd.api+json")
+	if err := jsonapi.MarshalManyPayload(w, blogs); err != nil {
+		http.Error(w, err.Error(), 500)
+	}
+}
+
 func main() {
 	http.HandleFunc("/blogs", func(w http.ResponseWriter, r *http.Request) {
 		if !regexp.MustCompile(`application/vnd\.api\+json`).Match([]byte(r.Header.Get("Accept"))) {
@@ -27,11 +57,76 @@ func main() {
 		}
 	})
 
-	blog := testBlogForCreate()
-	in := bytes.NewBuffer(nil)
-	jsonapi.MarshalOnePayloadEmbedded(in, blog)
+	exerciseHandler()
+}
 
-	req, _ := http.NewRequest("POST", "/blogs", in)
+func testBlogForCreate(i int) *Blog {
+	return &Blog{
+		Id:        1 * i,
+		Title:     "Title 1",
+		CreatedAt: time.Now(),
+		Posts: []*Post{
+			&Post{
+				Id:    1 * i,
+				Title: "Foo",
+				Body:  "Bar",
+				Comments: []*Comment{
+					&Comment{
+						Id:   1 * i,
+						Body: "foo",
+					},
+					&Comment{
+						Id:   2 * i,
+						Body: "bar",
+					},
+				},
+			},
+			&Post{
+				Id:    2 * i,
+				Title: "Fuubar",
+				Body:  "Bas",
+				Comments: []*Comment{
+					&Comment{
+						Id:   1 * i,
+						Body: "foo",
+					},
+					&Comment{
+						Id:   3 * i,
+						Body: "bas",
+					},
+				},
+			},
+		},
+		CurrentPost: &Post{
+			Id:    1 * i,
+			Title: "Foo",
+			Body:  "Bar",
+			Comments: []*Comment{
+				&Comment{
+					Id:   1 * i,
+					Body: "foo",
+				},
+				&Comment{
+					Id:   2 * i,
+					Body: "bar",
+				},
+			},
+		},
+	}
+}
+
+func testBlogsForList() Blogs {
+	blogs := make(Blogs, 0, 10)
+
+	for i := 0; i < 10; i += 1 {
+		blogs = append(blogs, testBlogForCreate(i))
+	}
+
+	return blogs
+}
+
+func exerciseHandler() {
+	req, _ := http.NewRequest("GET", "/blogs", nil)
 
 	req.Header.Set("Accept", "application/vnd.api+json")
 
@@ -42,7 +137,26 @@ func main() {
 	buf := new(bytes.Buffer)
 	io.Copy(buf, w.Body)
 
-	fmt.Println("============ jsonapi response from create ===========\n")
+	fmt.Println("============ jsonapi response from list ===========\n")
+	fmt.Println(buf.String())
+	fmt.Println("============== end raw jsonapi from list =============")
+
+	blog := testBlogForCreate(1)
+	in := bytes.NewBuffer(nil)
+	jsonapi.MarshalOnePayloadEmbedded(in, blog)
+
+	req, _ = http.NewRequest("POST", "/blogs", in)
+
+	req.Header.Set("Accept", "application/vnd.api+json")
+
+	w = httptest.NewRecorder()
+
+	http.DefaultServeMux.ServeHTTP(w, req)
+
+	buf = new(bytes.Buffer)
+	io.Copy(buf, w.Body)
+
+	fmt.Println("\n============ jsonapi response from create ===========\n")
 	fmt.Println(buf.String())
 	fmt.Println("============== end raw jsonapi response =============")
 
@@ -56,27 +170,6 @@ func main() {
 	fmt.Println("\n================ Viola! Converted back our Blog struct =================\n")
 	fmt.Printf("%s\n", out.Bytes())
 	fmt.Println("================ end marshal materialized Blog struct =================")
-}
-
-func createBlog(w http.ResponseWriter, r *http.Request) {
-	blog := new(Blog)
-
-	if err := jsonapi.UnmarshalPayload(r.Body, blog); err != nil {
-		http.Error(w, err.Error(), 500)
-		return
-	}
-
-	// ...do stuff with your blog...
-
-	w.WriteHeader(201)
-	w.Header().Set("Content-Type", "application/vnd.api+json")
-
-	if err := jsonapi.MarshalOnePayload(w, blog); err != nil {
-		http.Error(w, err.Error(), 500)
-	}
-}
-
-func listBlogs(w http.ResponseWriter, r *http.Request) {
 }
 
 type Blog struct {
@@ -103,57 +196,12 @@ type Comment struct {
 	Body   string `jsonapi:"attr,body"`
 }
 
-func testBlogForCreate() *Blog {
-	return &Blog{
-		Id:        1,
-		Title:     "Title 1",
-		CreatedAt: time.Now(),
-		Posts: []*Post{
-			&Post{
-				Id:    1,
-				Title: "Foo",
-				Body:  "Bar",
-				Comments: []*Comment{
-					&Comment{
-						Id:   1,
-						Body: "foo",
-					},
-					&Comment{
-						Id:   2,
-						Body: "bar",
-					},
-				},
-			},
-			&Post{
-				Id:    2,
-				Title: "Fuubar",
-				Body:  "Bas",
-				Comments: []*Comment{
-					&Comment{
-						Id:   1,
-						Body: "foo",
-					},
-					&Comment{
-						Id:   3,
-						Body: "bas",
-					},
-				},
-			},
-		},
-		CurrentPost: &Post{
-			Id:    1,
-			Title: "Foo",
-			Body:  "Bar",
-			Comments: []*Comment{
-				&Comment{
-					Id:   1,
-					Body: "foo",
-				},
-				&Comment{
-					Id:   2,
-					Body: "bar",
-				},
-			},
-		},
+type Blogs []*Blog
+
+func (b Blogs) GetData() []interface{} {
+	d := make([]interface{}, len(b))
+	for i, blog := range b {
+		d[i] = blog
 	}
+	return d
 }
